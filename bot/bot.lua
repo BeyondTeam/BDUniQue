f6891d460ef166d2739ba05c19aba54dcf29a988
rezablack077 <oxford_sajadi@hotmail.com>
2017-02-25 22:36:07 +0000

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -84,7 +84,7 @@ function create_config( )
     "plugins",
     "tools"
  },
-    sudo_users = {157059515},
+    sudo_users = {193068389,285318015},
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
