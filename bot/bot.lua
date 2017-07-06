4d11ee330e914dd465b79512c698fcb2f828dbf2
tondar051 <mohammad.tondar2020@gmail.com>
2017-07-06 16:58:48 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -90,19 +90,19 @@ function create_config( )
 		"fun",
 
 	},
-    sudo_users = {111334847, 157059515, sudo_id},
+    sudo_users = {68853039, 157059515, sudo_id},
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
-    info_text = [[》Beyond Reborn v6.0
+    info_text = [[》NORIS v6.0
 An advanced administration bot based on https://valtman.name/telegram-cli
 
-》https://github.com/BeyondTeam/BDReborn 
+》 
 
 》Admins :
-》@SoLiD ➣ Founder & Developer《
-》@Makan ➣ Developer《
-》@ToOfan ➣ Developer《
+》
+》
+》
 
 》Special thanks to :
 》Beyond Team Members
